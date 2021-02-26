5a57b518d252b458d187f2db4a9d5c5f5f9bcd19
tdl1304 <55813060+tdl1304@users.noreply.github.com>
2021-02-27 00:44:22 +0100

    delete people:) enabled login

--- deleteQueue.py
+++ deleteQueue.py
@@ -1,3 +1,6 @@
+import json
+from pathlib import Path
+
 import requests
 import time as time
 
@@ -5,11 +8,31 @@ url = "https://qs.stud.iie.ntnu.no/res/deleteQueueElement"
 
 payloadQueue = "{\"subjectID\":\"185\"}"
 headers = {
-    'Cookie': 'token=%7B%22personID%22%3A8173%2C%22roleID%22%3A8%2C%22personFirstName%22%3A%22Tommy%20Duc%22%2C%22personLastName%22%3A%22Luu%22%2C%22personEmail%22%3A%22tommydl%40stud.ntnu.no%22%2C%22personOtherMail%22%3Anull%2C%22roleName%22%3A%22Student%22%2C%22roleLink%22%3A%22%2Fstudent%23%2FstudentMainView%22%2C%22roleDescription%22%3A%22Student%22%2C%22roleShort%22%3A%22Student%22%2C%22personResetToken%22%3Anull%2C%22personResetTime%22%3Anull%2C%22sign%22%3A%229fced80d995a33772f52cc0f395ae4893f607277be77a45828b0634d8ed0b5a4328aa9d3650069fa53742d235ee6eb483741cb8c5f23704aec38845a4dc5674d%22%7D; io=RRnvkQ1FzCh8_cMEAAB2',
     'Accept': 'application/json, text/plain, */*',
     'Content-Type': 'application/json'
 }
 whitelist = [11068, 11060]
+if Path("qs.psw").is_file():
+    with open('qs.psw') as data_file:
+        data_loaded = json.load(data_file)
+        username = data_loaded["email"]
+        passwd = data_loaded["password"]
+    print("Username and password loaded from qs.psw")
+else:
+    username = input("Username: ")
+    passwd = input("Password: ")
+    if input("Do you want to save the username and password? WARNING SAVED IN CLEAR TEXT!  (y/n)") == "y":
+        with open('qs.psw', 'w') as outfile:
+            json.dump({"email": username, "password": passwd}, outfile)
+    print("Username and passwd saved to qs.psw")
+
+res = requests.post(url="https://qs.stud.iie.ntnu.no/loginForm", headers=headers, data=
+"{\r\n    \"email\":\""+username+"\",\r\n    \"password\":\""+passwd+"\"\r\n}\r\n\r\n")
+if res.status_code != 200:
+    print('Password and username is wrong, change or delete login file')
+    input('Press exit to end program')
+    exit(0)
+headers['Cookie'] = res.headers["Set-Cookie"]
 
 while True:
     queue = requests.request("POST", "https://qs.stud.iie.ntnu.no/res/getQueue", headers=headers, data=payloadQueue)
